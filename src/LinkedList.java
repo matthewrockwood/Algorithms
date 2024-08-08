ba0ba04a6bf2a1f69534db238ceadf70210e024c
matthewrockwood <rockwoodmatthew@gmail.com>
2024-08-08 18:44:20 -0400

    6th commit

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,4 +1,5 @@
 public class LinkedList {
+    public Node head;
     public class Node{
         Node next;
         int data;
@@ -6,6 +7,18 @@ public class LinkedList {
         Node(int data){
             this.data = data;
             this.next = null;
+        }
+    }
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
         }
     }
     public Node reverseList(Node head){
@@ -22,5 +35,31 @@ public class LinkedList {
 
         }
         return prev;
+    }
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public int binaryToDecimal(Node head){
+        int ans = 0;
+        int[] arr = new int[35];
+        Node curr = head;
+        int i = 0;
+        while(curr!= null){
+            arr[i++] = curr.data;
+            curr=curr.next;
+        }
+        int x =0;
+        for(int j=i;j>=0;j--){
+            if(arr[j]==1) {
+                ans += Math.pow(2, x);
+            }
+            x++;
+        }
+    return ans;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.sql.SQLOutput;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        printReverseString("Hello");
+
+        /* Adding binary digits to the linked list
+        list.add(1);
+        list.add(0);
+        list.add(1);
+        list.add(1);
+        int ans = list.binaryToDecimal(list.head);
+        System.out.println("Ans " + ans);
        ArrayQueue que = new ArrayQueue();
        que.enqueue(1);
         que.enqueue(1);
@@ -10,12 +23,27 @@ public class Main {
 
 
 
-       // int array[] = new int[]{12,4,67,43,55,3,2,28,13};
-       // bubbbleSort(array);
-       // for (int i = 0; i < array.length; i++) {
-           // System.out.println(array[i]);
 
-       // }
+
+        int array[] = new int[]{12,4,67,43,55,3,2,28,13};
+        bubbbleSort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+
+       }
+
+         */
+    }
+    public static void printReverseString(String x){
+        Stack<Character> stack = new Stack<>();
+        for(int i =0; i<x.length(); i++){
+            stack.push(x.charAt(i));
+        }
+        String ans = "";
+        while(!stack.empty()){
+            ans+= stack.pop();
+        }
+        System.out.println(ans);
     }
     public static int[] swap(int[] arr, int indx1, int indx2){
         int temp = arr[indx1];
